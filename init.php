c99aa537d9bfe87c3f3c79aba3d8b0aba8c2e0e1
hellowpio <hellowpio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-12-20 19:26:48 +0000

    0.0.11b
    git-svn-id: http://plugins.svn.wordpress.org/magyar-eszkozok-divi-hez/trunk@2215906 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ init.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Plugin Name: Magyar kiegészítések Divi-hez
+Plugin URI:  https://hellowp.io
+Description: Magyar Divi beállítások (fordítás, megjelenés és egyéb fejlesztések)
+=======
+Version:     0.0.11
+Author:      HelloWP.io
+Author URI:  https://hellowp.io/hu
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+define('MAGYAR_DIVI_DIR', trailingslashit(dirname(__FILE__)));
+include_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+//load modules
+
+if (wp_get_theme() == 'Divi' || wp_get_theme()->parent() == 'Divi' || strpos(@$theme->Name, 'Divi') !== false) {
+// felülírjuk az eredeti magyar fordítáast
+
+    add_action('plugins_loaded', 'load_magyar_nyelv_divi');
+// szinkronizáljuk a WordPress dátumformátumával a Divi-t
+    add_action('plugins_loaded', 'load_magyar_datum_divi');
+
+    function load_magyar_nyelv_divi()
+    {
+
+        load_plugin_textdomain('Divi', false, dirname(plugin_basename(__FILE__)) . '/lang/');
+        load_plugin_textdomain('et_builder', false, dirname(plugin_basename(__FILE__)) . '/lang/');
+    }
+
+    function load_magyar_datum_divi()
+    {
+        $get_divi_date_format                     = get_option('et_divi');
+        $get_divi_date_format['divi_date_format'] = get_option('date_format');
+        update_option('et_divi', $get_divi_date_format);
+    }
+
+//add_action('admin_menu', 'divi_magyarul_menu', 30);
+    function divi_magyarul_menu()
+    {
+        global $page_options;
+        add_submenu_page('et_divi_options', __('Divi magyarul', 'divi-magyarul'), __('Divi magyarul', 'divi-magyarul'), 'manage_options', 'divi_magyarul', 'divi_magyarul');
+    }
+
+}
+
+if (wp_get_theme() == 'Extra' || wp_get_theme()->parent() == 'Extra' || strpos(@$theme->Name, 'Extra') !== false) {
+
+    add_action('plugins_loaded', 'load_magyar_nyelv_extra');
+
+    function load_magyar_nyelv_extra()
+    {
+        load_plugin_textdomain('extra', false, dirname(plugin_basename(__FILE__)) . '/lang/');
+        load_plugin_textdomain('Extra', false, dirname(plugin_basename(__FILE__)) . '/lang/Extra/');
+        load_plugin_textdomain('et_builder', false, dirname(plugin_basename(__FILE__)) . '/lang/');
+        load_plugin_textdomain('et-core', false, dirname(plugin_basename(__FILE__)) . '/lang/');
+
+    }
+}
+
+if (is_plugin_active('monarch/monarch.php')) {
+    load_plugin_textdomain('Monarch', false, dirname(plugin_basename(__FILE__)) . '/lang/');
+}
+
+if (is_plugin_active('bloom/bloom.php')) {
+    load_plugin_textdomain('bloom', false, dirname(plugin_basename(__FILE__)) . '/lang/');
+}
---
+++ readme.txt
@@ -0,0 +1,64 @@
+=== Magyar kiegészítésék Divi-hez ===
+
+Contributors: hellowpio, wphuorg    
+Tags: divi, magyar
+Requires PHP: 5.6  
+Requires at least: 3.0  
+Tested up to: 5.2.5  
+
+Stable tag: 0.0.11
+
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html 
+
+DIVI magyar kiegészítések és fordítás 
+
+
+== Description ==
+
+Magyar Divi beállítások (fordítás, megjelenés és egyéb fejlesztések)
+
+
+== Installation ==
+
+1. Töltsd le a bővítményt
+2. Wordpress-ben bővítmények / új hozzáadása menüben fel kell tölteni
+3. Működik
+
+== Frequently Asked Questions ==
+
+= Miben tér el a fordítás az eredeti DIVI-től? =
+
+A DIVI fordítása sok Google fordítást tartalmaz, ami sok esetben érthetetlen és magyartalan. A bővítmény ezeket a hibákat igyekszik orvosolni.
+
+== Changelog ==
+
+= 0.0.11 =
+* Új fordítások, DIVI 4
+
+= 0.0.10 =
+* Hibajavítás
+
+= 0.0.9 =
+* Hibajavítás
+
+= 0.0.8 =
+* Hibajavítás
+
+= 0.0.7 =
+* Hibajavítás
+
+= 0.0.6 =
+* Hibajavítás
+
+= 0.0.5 =
+* Új fordítások: Bloom, Monarch, Extra
+
+= 0.0.4 =
+* Hibajavítás
+
+= 0.0.3 =
+* Trademark megfelelés
+
+= 0.0.2 =
+* WordPress.org -ra készült első verzió
